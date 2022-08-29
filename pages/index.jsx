3db28ad61745597f46133a7578d32c4725ce8989
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-29 17:47:16 -0400

    Setted up a basic Next package.

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function Home() {
+    return (<>
+            <h1>Hello!</h1>
+            <p>This is a simple website created with Next.js!</p>
+        </>);
+}
+exports.default = Home;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Hello!</h1>
+            <p>This is a simple website created with Next.js!</p>
+        </>
+    )
+}
