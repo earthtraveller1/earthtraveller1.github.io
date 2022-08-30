0d554c2c52eb334ce5fc1d4757222bd84712ed59
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-08-30 12:55:17 -0400

    Added a title for the page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
-export default function Home() {
+import Head from 'next/head'
+
+export default function Index() {
     return (
         <>
+            <Head>
+                <title>EarthTraveller1</title>
+            </Head>
+            
             <h1>Hello!</h1>
             <p>This is a simple website created with Next.js!</p>
         </>
