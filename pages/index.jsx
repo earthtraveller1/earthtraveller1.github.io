8aa4857e19ae16b9756b56fcd54fe56d39fdc49b
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-08-30 13:13:46 -0400

    Removed uneeded JavaScript file.

--- pages/index.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Home() {
-    return (<>
-            <h1>Hello!</h1>
-            <p>This is a simple website created with Next.js!</p>
-        </>);
-}
-exports.default = Home;
