50e885750d043d52389699a783efd856219945dc
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-08-30 13:08:07 -0400

    Ported the old website onto Next.js.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import Head from 'next/head'
 
+import styles from './index.module.css'
+
 export default function Index() {
     return (
         <>
+        <div className={styles.page}>
             <Head>
                 <title>EarthTraveller1</title>
             </Head>
             
-            <h1>Hello!</h1>
-            <p>This is a simple website created with Next.js!</p>
+            <h1 className={styles.center + " " + styles.bigText}>NO</h1>
+            <p className={styles.center + " " + styles.smallText}>Just no...</p>
+        </div>
         </>
     )
 }
