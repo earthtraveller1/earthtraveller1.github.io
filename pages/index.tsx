db62404b60e499f81473409a6b86208b87ae4f6a
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-14 18:22:33 -0500

    Got rid of the old page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,25 @@
 import Head from 'next/head'
-import Script from 'next/script'
+import Link from 'next/link'
 
 import React from 'react'
 
-import styles from './index.module.css'
-
 export default function Index() {
-    const [smallText, setSmallText] = React.useState("")
-    
-    React.useEffect(() => {
-        setTimeout(() => {
-            setSmallText("Just no...")
-        }, 2500)
-    }, [])
-    
     return (
         <>
-        <div className={styles.page}>
-            <Head>
-                <title>EarthTraveller1</title>
-            </Head>
-            
-            <h1 className={styles.center + " " + styles.bigText}>NO</h1>
-            <p className={styles.center + " " + styles.smallText}>{smallText}</p>
-        </div>
+        <Head>
+            <title>EarthTraveller1</title>
+        </Head>
+        
+        <h1>EarthTraveller1</h1>
+        
+        <p>
+            This webpage is created with Next.js, and hosted on GitHub pages. Below
+            is a table of contents for this site.
+        </p>
+        
+        <ol>
+            <li><Link href="/ccxx-project-conventions">C/C++ Project Conventions</Link></li>
+        </ol>
         </>
     )
 }
