5e14ef6daa4b73c0da6b63662b53e52822d3bf76
Yue <liangyuezhao7@gmail.com>
2023-08-24 18:57:00 -0400

    Created a basic Next.js project.

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+    return <main>
+        <h1 className="text-center text-4xl m-4">EarthTraveller1</h1>
+        <p className="text-center text-md m-2">Yes, Indeed!</p>
+    </main>
+}
